15a424bd3d8ec9e5dd3dba46a874ac2c18038c48
nfsmw15 <laratos@nfsmw15.de>
2026-05-01 19:35:55 +0200

    Replace placehold.it with local system/placeholder.php (no external dependency)

--- templates/bootstrap/services.php
+++ templates/bootstrap/services.php
@@ -21,7 +21,7 @@
         <!-- Image Header -->
         <div class="row">
             <div class="col-lg-12">
-                <img class="img-responsive" src="http://placehold.it/1200x300" alt="">
+                <img class="img-responsive" src="./system/placeholder.php?w=1200&h=300" alt="">
             </div>
         </div>
         <!-- /.row -->
