7310f9f9df3705b48951eca32fe5a4057a49207b
nfsmw15 <laratos@nfsmw15.de>
2026-05-26 01:05:44 +0200

    Fix CSP: Inline-Event-Handler in userlist.php und regist.php entfernt
    - userlist.php: 4x onClick=window.history.back() → data-history-back Attribut
    - regist.php: captcha onClick entfernt
    - app.js: jQuery-Handler für captcha-img und data-history-back ergänzt

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 $(function () {
     $('.carousel').carousel({ interval: 5000 });
+
+    // Captcha refresh on click
+    $('.captcha-img').css('cursor', 'pointer').on('click', function () {
+        $(this).attr('src', '?captcha=img&generate=' + Math.random());
+    });
+
+    // History back buttons
+    $('[data-history-back]').on('click', function (e) {
+        e.preventDefault();
+        window.history.back();
+    });
 });
