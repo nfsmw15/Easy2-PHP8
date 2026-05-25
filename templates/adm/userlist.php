b96cbd5a5b2567a0355fba84363da44f18f44f4d
nfsmw15 <laratos@nfsmw15.de>
2026-05-26 01:05:04 +0200

    Fix CSP: Inline-Event-Handler in userlist.php und regist.php entfernt
    - userlist.php: 4x onClick=window.history.back() → data-history-back Attribut
    - regist.php: captcha onClick entfernt
    - app.js: jQuery-Handler für captcha-img und data-history-back ergänzt
    
    CHANGELOG: v1.1.4 — OSM, CSP-Fixes dokumentiert

--- templates/adm/userlist.php
+++ templates/adm/userlist.php
@@ -206,7 +206,7 @@
 								echo $additional_fields->showFields(0, 'edit-', $id);
 							?>
                            <div class="form-group">
-                                <a class="btn btn-warning pull-left" href="#" onClick="window.history.back();"><i class="fa fa-times"></i> Abbrechen</a>
+                                <a class="btn btn-warning pull-left" href="#" data-history-back><i class="fa fa-times"></i> Abbrechen</a>
                                 <button class="btn btn-success pull-right" type="submit"><i class="fa fa-floppy-o"></i> Speichern</button>
                             </div>
                         </form>
@@ -232,7 +232,7 @@
                                 <input type="password" class="form-control" name="delete-password" maxlength="64" placeholder="Passwort" required>
                             </div>
                             <div class="form-group">
-                                <a class="btn btn-success pull-left" href="#" onClick="window.history.back();"><i class="fa fa-times"></i> Abbrechen</a>
+                                <a class="btn btn-success pull-left" href="#" data-history-back><i class="fa fa-times"></i> Abbrechen</a>
                                 <button class="btn btn-danger pull-right" type="submit"><i class="fa fa-trash"></i> L&ouml;schen</button>
                             </div>
                         </form>
@@ -258,7 +258,7 @@
                                 <input type="password" class="form-control" name="reset-password" maxlength="64" placeholder="Passwort" required>
                             </div>
                             <div class="form-group">
-                               	<a class="btn btn-success pull-left" href="#" onClick="window.history.back();"><i class="fa fa-times"></i> Abbrechen</a>
+                               	<a class="btn btn-success pull-left" href="#" data-history-back><i class="fa fa-times"></i> Abbrechen</a>
                                 <button class="btn btn-danger pull-right" type="submit"><i class="fa fa-refresh"></i> Zur&uuml;cksetzen</button>
                             </div>
                         </form>
@@ -284,7 +284,7 @@
                                 <input type="password" class="form-control" name="reset-password" maxlength="64" placeholder="Passwort" required>
                             </div>
                             <div class="form-group">
-                                <a class="btn btn-success pull-left" href="#" onClick="window.history.back();"><i class="fa fa-times"></i> Abbrechen</a>
+                                <a class="btn btn-success pull-left" href="#" data-history-back><i class="fa fa-times"></i> Abbrechen</a>
                                 <button class="btn btn-danger pull-right" type="submit"><i class="fa fa-eraser"></i> Entfernen</button>
                             </div>
                         </form>
