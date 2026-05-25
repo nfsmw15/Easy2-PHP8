9c71ae5ebdff69f2e38e8fd86049c899e03608d5
nfsmw15 <laratos@nfsmw15.de>
2026-05-25 19:21:47 +0200

    Add dual layout system: navbar and dashboard/sidebar switchable via admin settings

---
+++ templates/layout/footer_navbar.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <hr>
+    <footer class="py-3">
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="text-muted">Copyright &copy; <?php echo date('Y'); ?></p>
+            </div>
+        </div>
+    </footer>
+</div>
---
+++ templates/layout/header_navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="./"><?php echo htmlspecialchars((string)$loginsystem->getMainData('site_title'), ENT_QUOTES, 'UTF-8'); ?></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#bs-navbar" aria-controls="bs-navbar" aria-expanded="false" aria-label="Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="bs-navbar">
+            <ul class="navbar-nav ms-auto">
+                <?php echo $menu->getMenu(); ?>
+            </ul>
+        </div>
+    </div>
+</nav>
