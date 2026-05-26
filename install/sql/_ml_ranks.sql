684d3789abbbe6eece55e146570c69afa1ebdbeb
nfsmw15 <laratos@nfsmw15.de>
2026-05-26 13:35:40 +0200

    Fix Mitglied rank: remove menu (18) and additional_fields (19) from allowed sites
    Normal members could access ?p=menu and ?p=additional_fields (admin pages).
    Migration for existing installs in CHANGELOG.

--- install/sql/_ml_ranks.sql
+++ install/sql/_ml_ranks.sql
@@ -14,7 +14,7 @@ CREATE TABLE `[prefix]_ml_ranks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `[prefix]_ml_ranks` (`id`, `title`, `sites`, `default`, `rules`, `pos`, `guest`, `color`, `special`) VALUES
-(1813201540, 'Mitglied', '9,21,22,23,8,26,24,2,3,10,15,16,17,18,19,27,4,5,25,m10,m9', 1, '', 2, 0, '3d8a00', ''),
+(1813201540, 'Mitglied', '9,21,22,23,8,26,24,2,3,10,15,16,17,27,4,5,25,m10,m9', 1, '', 2, 0, '3d8a00', ''),
 (1813201549, 'Administrator', '9,34,21,22,23,8,26,24,2,3,10,30,15,16,17,18,19,27,4,1,11,5,6,25,12,7,m7,m11,m10,m9', 0, 'fields_add,fields_edit,fields_remove,mainsave,menu_add,menu_edit,menu_fill_gaps,menu_remove,menu_reset_pos,rank_default,rank_delete,rank_edit,rank_move,rank_new,site_add,site_edit,site_remove,user_add,user_delete,user_disable,user_edit,user_enable,user_pwreset,user_rank,user_rm_avatar,user_show', 1, 0, '', 'bold'),
 (1813201541, 'Webmaster', 'all', 0, 'all', 0, 0, 'aa0000', 'bold'),
 (1813201542, 'Gast', '9,21,22,23,8,26,24,2,3,29,15,16,17,19,27,13,35,14,5,25,m19', 0, '', 3, 1, '', '');
