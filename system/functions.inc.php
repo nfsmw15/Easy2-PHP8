f10b8697c5673ed8a18e1cd3417ad225ed0b3afe
nfsmw15 <laratos@nfsmw15.de>
2026-05-26 09:12:18 +0200

    Bump version to 1.2.3

--- system/functions.inc.php
+++ system/functions.inc.php
@@ -18,7 +18,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  *********************************************/
 
-define('EASY_VERSION', '1.2.1');
+define('EASY_VERSION', '1.2.3');
 
 // ─── Input-Sanitierung ──────────────────────────────────────────────────────
 
