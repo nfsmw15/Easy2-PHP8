cdd0dc7b10d10b0351cc81e64156f66baa959d87
nfsmw15 <laratos@nfsmw15.de>
2026-05-27 16:46:02 +0200

    Fix: escape all POST values in settings form (XSS/HTML-attribute-injection)
    - e() applied to all $_POST values in value="" attributes
    - HTML-encoded-stored fields (title, email, smtp_host etc.): two-path (e() on POST, DB direct)
    - Raw-stored fields (impressum_info, osm_embed_url): uniform e() on both
    - Summernote HTML editors intentionally kept unescaped

--- templates/adm/settings.php
+++ templates/adm/settings.php
@@ -61,7 +61,7 @@
 									<label for="title">Seitentitel:</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-font fa-fw"></i></span>
-										<input type="text" name="title" require class="form-control" value="<?php echo isset($_POST['title']) ? $_POST['title'] : $loginsystem->getMainData('site_title'); ?>" id="title" maxlength="64" placeholder="Seitentitel">
+										<input type="text" name="title" require class="form-control" value="<?php echo isset($_POST['title']) ? e($_POST['title']) : $loginsystem->getMainData('site_title'); ?>" id="title" maxlength="64" placeholder="Seitentitel">
 									</div>
 								</div>
 								<div class="col-sm-6"><br>
@@ -76,7 +76,7 @@
 									<label for="title_short">Seitentitel (kurz):</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-font fa-fw"></i></span>
-										<input type="text" name="title_short" require class="form-control" value="<?php echo isset($_POST['title_short']) ? $_POST['title_short'] : $loginsystem->getMainData('short_site_title'); ?>" id="title_short" maxlength="16" placeholder="Seitentitel (kurz)">
+										<input type="text" name="title_short" require class="form-control" value="<?php echo isset($_POST['title_short']) ? e($_POST['title_short']) : $loginsystem->getMainData('short_site_title'); ?>" id="title_short" maxlength="16" placeholder="Seitentitel (kurz)">
 									</div>
 								</div>
 								<div class="col-sm-6">
@@ -91,7 +91,7 @@
 									<label for="email">Administrator E-Mail:</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-envelope fa-fw"></i></span>
-										<input type="email" name="email" require class="form-control" value="<?php echo isset($_POST['email']) ? $_POST['email'] : $loginsystem->getMainData('administrator_mail'); ?>" id="email" maxlength="128" placeholder="Administrator E-Mail">
+										<input type="email" name="email" require class="form-control" value="<?php echo isset($_POST['email']) ? e($_POST['email']) : $loginsystem->getMainData('administrator_mail'); ?>" id="email" maxlength="128" placeholder="Administrator E-Mail">
 									</div>
 								</div>
 								<div class="col-sm-6">
@@ -106,7 +106,7 @@
 									<label for="from">Absender E-Mail: (System E-Mails)</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-envelope fa-fw"></i></span>
-										<input type="email" name="from" require class="form-control" value="<?php echo isset($_POST['from']) ? $_POST['from'] : $loginsystem->getMainData('mail_sender'); ?>" id="from" maxlength="128" placeholder="Sender E-Mail">
+										<input type="email" name="from" require class="form-control" value="<?php echo isset($_POST['from']) ? e($_POST['from']) : $loginsystem->getMainData('mail_sender'); ?>" id="from" maxlength="128" placeholder="Sender E-Mail">
 									</div>
  								</div>
 								<div class="col-sm-6"><br>
@@ -121,7 +121,7 @@
 									<label for="to">Empf&auml;nger E-Mail: (Antworten auf System E-Mails)</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-envelope fa-fw"></i></span>
-										<input type="email" name="to" require class="form-control" value="<?php echo isset($_POST['to']) ? $_POST['to'] : $loginsystem->getMainData('mail_receiver'); ?>" id="to" maxlength="128" placeholder="Empf&auml;nger E-Mail">
+										<input type="email" name="to" require class="form-control" value="<?php echo isset($_POST['to']) ? e($_POST['to']) : $loginsystem->getMainData('mail_receiver'); ?>" id="to" maxlength="128" placeholder="Empf&auml;nger E-Mail">
 									</div>
  								</div>
 								<div class="col-sm-6"><br>
@@ -135,11 +135,11 @@
 							<div class="row">
 								<div class="col-sm-4">
 									<label for="smtp_host">SMTP Host:</label>
-									<input type="text" name="smtp_host" class="form-control" value="<?php echo isset($_POST['smtp_host']) ? $_POST['smtp_host'] : $loginsystem->getMainData('smtp_host'); ?>" id="smtp_host" maxlength="128" placeholder="z.B. mail.nfsmw15.de">
+									<input type="text" name="smtp_host" class="form-control" value="<?php echo isset($_POST['smtp_host']) ? e($_POST['smtp_host']) : $loginsystem->getMainData('smtp_host'); ?>" id="smtp_host" maxlength="128" placeholder="z.B. mail.nfsmw15.de">
 								</div>
 								<div class="col-sm-2">
 									<label for="smtp_port">Port:</label>
-									<input type="number" name="smtp_port" class="form-control" value="<?php echo isset($_POST['smtp_port']) ? $_POST['smtp_port'] : $loginsystem->getMainData('smtp_port'); ?>" id="smtp_port" placeholder="587">
+									<input type="number" name="smtp_port" class="form-control" value="<?php echo isset($_POST['smtp_port']) ? e($_POST['smtp_port']) : $loginsystem->getMainData('smtp_port'); ?>" id="smtp_port" placeholder="587">
 								</div>
 								<div class="col-sm-3">
 									<label for="smtp_encryption">Verschl&uuml;sselung:</label>
@@ -155,7 +155,7 @@
 							<div class="row">
 								<div class="col-sm-4">
 									<label for="smtp_user">SMTP Benutzername:</label>
-									<input type="text" name="smtp_user" class="form-control" value="<?php echo isset($_POST['smtp_user']) ? $_POST['smtp_user'] : $loginsystem->getMainData('smtp_user'); ?>" id="smtp_user" maxlength="128" placeholder="user@nfsmw15.de">
+									<input type="text" name="smtp_user" class="form-control" value="<?php echo isset($_POST['smtp_user']) ? e($_POST['smtp_user']) : $loginsystem->getMainData('smtp_user'); ?>" id="smtp_user" maxlength="128" placeholder="user@nfsmw15.de">
 								</div>
 								<div class="col-sm-4">
 									<label for="smtp_pass">SMTP Passwort:</label>
@@ -170,7 +170,7 @@
 										<label for="dsgvo">E-Mail f&uuml;r Datenschutz:</label>
 										<div class="form-group input-group">
 											<span class="input-group-text"><i class="fa fa-fw fa-envelope fa-fw"></i></span>
-											<input type="email" name="dsgvo" require class="form-control" value="<?php echo isset($_POST['dsgvo']) ? $_POST['dsgvo'] : $loginsystem->getMainData('dsgvo_email'); ?>" id="dsgvo" maxlength="128" placeholder="E-Mail f&uuml;r Datenschutz">
+											<input type="email" name="dsgvo" require class="form-control" value="<?php echo isset($_POST['dsgvo']) ? e($_POST['dsgvo']) : $loginsystem->getMainData('dsgvo_email'); ?>" id="dsgvo" maxlength="128" placeholder="E-Mail f&uuml;r Datenschutz">
 										</div>
 									</div>
 									<div class="col-sm-6"><br>
@@ -203,7 +203,7 @@
 									<div class="col-sm-6">
 										<label for="impressum_info">Impressums Angaben:</label>
 										<div class="form-group">
-											<textarea name="impressum_info" id="impressum_info" class="form-control" rows="6"><?php echo isset($_POST['impressum_info']) ? $_POST['impressum_info'] : $loginsystem->getMainData('impressum_info'); ?></textarea>
+											<textarea name="impressum_info" id="impressum_info" class="form-control" rows="6"><?php echo e(isset($_POST['impressum_info']) ? $_POST['impressum_info'] : (string)$loginsystem->getMainData('impressum_info')); ?></textarea>
 										</div>
 									</div>
 									<div class="col-sm-6"><br>
@@ -217,6 +217,7 @@
 									<div class="col-sm-12">
 										<label for="impressum_content">Impressumsinhalt (Haftungsausschluss):</label>
 										<div class="form-group">
+											<?php /* Summernote-HTML-Editor: Inhalte werden bewusst als HTML ausgegeben. Eingabe erfolgt nur durch Admins mit mainsave-Recht. */ ?>
 											<textarea name="impressum_content" id="impressum_content" class="form-control summernote" rows="6"><?php echo isset($_POST['impressum_content']) ? html_entity_decode($_POST['impressum_content'], ENT_QUOTES, 'UTF-8') : html_entity_decode($loginsystem->getImpressum(), ENT_QUOTES, 'UTF-8'); ?></textarea>
 										</div>
 									</div>
@@ -228,6 +229,7 @@
 									<div class="col-sm-12">
 										<label for="privacy_policy">Datenschutzerkl&auml;rung:</label>
 										<div class="form-group">
+											<?php /* Summernote-HTML-Editor: Inhalte werden bewusst als HTML ausgegeben. Eingabe erfolgt nur durch Admins mit mainsave-Recht. */ ?>
 											<textarea name="privacy_policy" id="privacy_policy" class="form-control summernote" rows="6"><?php echo isset($_POST['privacy_policy']) ? html_entity_decode($_POST['privacy_policy'], ENT_QUOTES, 'UTF-8') : html_entity_decode($loginsystem->getPrivacyPolicy(), ENT_QUOTES, 'UTF-8'); ?></textarea>
 										</div>
 									</div>
@@ -281,11 +283,11 @@
 									<label for="pwlength">Passwort min. L&auml;nge:</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-lock"></i></span>
-										<input type="number" class="form-control" id="pwlength" name="pwlength" placeholder="PW min. length" min="3" max="32" maxlength="2" value="<?php echo isset($_POST['password_length']) ? $_POST['password_length'] : $loginsystem->getMainData('password_length'); ?>">
+										<input type="number" class="form-control" id="pwlength" name="pwlength" placeholder="PW min. length" min="3" max="32" maxlength="2" value="<?php echo isset($_POST['pwlength']) ? e($_POST['pwlength']) : $loginsystem->getMainData('password_length'); ?>">
 									</div>
 								</div>
 								<div class="col-sm-6">
-									Hier muss die L&auml;nge angegeben werden, wie lang ein Passwort ein muss. Die maximale L&auml;nge eines Passwortes darf 64 Zeichen nicht &uuml;berschreiten. 
+									Hier muss die L&auml;nge angegeben werden, wie lang ein Passwort ein muss. Die maximale L&auml;nge eines Passwortes darf 64 Zeichen nicht &uuml;berschreiten.
 									Alle Zeichen dar&uuml;ber werden abgeschnitten.
 								</div>
 							</div>
@@ -344,10 +346,10 @@
 									</div>
 								</div>
 								<div class="col-sm-6">
-									Soll die Wiederherstellung von Benutzerdaten m&ouml;glich sein? Diese Option kann ganz ausgeschaltet werden, 
+									Soll die Wiederherstellung von Benutzerdaten m&ouml;glich sein? Diese Option kann ganz ausgeschaltet werden,
 									teilweise (Benutzer k&ouml;nnen Ihre eigenen &Auml;nderungen r&uuml;ckg&auml;ngig gemacht werden) eingeschaltet
-									oder ganz eingeschaltet. Bei der letzten Option, 
-									hat der Benutzer die M&ouml;glichkeit auch &Auml;nderungen von Admin's/Supportern o.&auml;. 
+									oder ganz eingeschaltet. Bei der letzten Option,
+									hat der Benutzer die M&ouml;glichkeit auch &Auml;nderungen von Admin's/Supportern o.&auml;.
 									r&uuml;ckg&auml;ngig zu machen (Au&szlig;nahme: Rang&auml;nderungen).
 								</div>
 							</div>
@@ -359,7 +361,7 @@
 									<label for="cookie_lifetime">"Eingeloggt bleiben" Luftzeit (in Sekunden):</label>
 									<div class="form-group input-group">
 										<span class="input-group-text"><i class="fa fa-fw fa-clock-o"></i></span>
-										<input type="number" class="form-control" id="cookie_lifetime" name="cookie_lifetime" placeholder="Zeit in Sekunden" min="86400" max="9999999999999999" maxlength="16" value="<?php echo isset($_POST['cookie_lifetime']) ? $_POST['cookie_lifetime'] : $loginsystem->getMainData('cookielifetime'); ?>">
+										<input type="number" class="form-control" id="cookie_lifetime" name="cookie_lifetime" placeholder="Zeit in Sekunden" min="86400" max="9999999999999999" maxlength="16" value="<?php echo isset($_POST['cookie_lifetime']) ? e($_POST['cookie_lifetime']) : $loginsystem->getMainData('cookielifetime'); ?>">
 									</div>
 									<div id="cookietime">entspricht: <?php echo number_format($loginsystem->getMainData('cookielifetime') / 86400, 2, ',', '.');?> Tagen</div>
 								</div>
@@ -395,7 +397,7 @@
 									<input type="url" class="form-control" id="osm_embed_url" name="osm_embed_url"
 										maxlength="512"
 										placeholder="https://www.openstreetmap.org/export/embed.html?..."
-										value="<?php echo isset($_POST['osm_embed_url']) ? htmlspecialchars($_POST['osm_embed_url']) : htmlspecialchars((string)$loginsystem->getMainData('osm_embed_url')); ?>">
+										value="<?php echo e(isset($_POST['osm_embed_url']) ? $_POST['osm_embed_url'] : (string)$loginsystem->getMainData('osm_embed_url')); ?>">
 								</div>
 								<div class="col-sm-6"><br>
 									URL aus dem OpenStreetMap-Share-Dialog. Auf
